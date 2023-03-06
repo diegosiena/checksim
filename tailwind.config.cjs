2067afdbe4d6fc5223439135ca243fe7996c9a9d
Diego Siena <dgosna@gmail.com>
2023-03-06 11:18:28 -0300

    chore: Setup Tailwind, postcss and google font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#537FE7',
+				black: '#181823'
+			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '4rem',
+					xl: '5rem',
+					'2xl': '6rem'
+				}
+			},
+			fontFamily: {
+				sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+				serif: ['Montserrat', ...defaultTheme.fontFamily.serif]
+			}
+		}
 	},
 	plugins: []
 };
